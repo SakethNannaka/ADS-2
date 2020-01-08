e365b1b33e7787dcf754d5eea741f6b85882371d
SakethNannaka <nannakasaisaketh@msitprogram.net>
2020-01-08 12:08:57 +0530

    Module-1

--- Module-1/PickTextFiles.java
+++ Module-1/PickTextFiles.java
@@ -3,44 +3,63 @@ import java.io.FileReader;
 import java.io.IOException;
 // import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+// import java.util.Arrays;
 import java.util.HashMap;
 
 public class PickTextFiles {
-    public void parseSynsets(String FileName) throws IOException{
+    /**
+     * 
+     */
+    HashMap <Integer ,ArrayList<String>>SynsetsMap = new HashMap<>();
+    HashMap <Integer ,Integer>HypernymsMap = new HashMap<>();
+    
+    /**
+     * 
+     * @param FileName
+     * @throws IOException
+     */
+        public void parseSynsets(String FileName) throws IOException{
             BufferedReader in = new BufferedReader(new FileReader(FileName));
         //Assuming you have a text file in D drive
         String strCurrentLine;
-        HashMap <Integer ,ArrayList<String>>map = new HashMap<>();
+        
         while ((strCurrentLine = in.readLine()) != null) {
             String []temp = strCurrentLine.split(",");
             ArrayList<String> vArrayList = new ArrayList<>();
-    
             int key = Integer.parseInt(temp[0]);
-            vArrayList.add(temp[1]);
+            String [] temp1 =temp[1].split(" ");
+            for (int i = 0; i < temp1.length; i++) {
+                vArrayList.add(temp1[i]);
+            }
             vArrayList.add(temp[2]);
-            map.put(key, vArrayList);
+            SynsetsMap.put(key, vArrayList);
         }
-        System.out.println("synsets : "+map.get(1));
+        System.out.println("synsets : "+SynsetsMap.get(1));
        
     }
+    /**
+     * 
+     * @param FileName1
+     * @throws IOException
+     */
    public void parseHypernyms(String FileName1) throws IOException{
         BufferedReader in = new BufferedReader(new FileReader("hypernyms.txt"));
         String strCurrentLine;
-        HashMap <Integer ,ArrayList<Integer>>map = new HashMap<>();
+        
     while ((strCurrentLine = in.readLine()) != null) {
         String []temp = strCurrentLine.split(",",2);
         int key = Integer.parseInt(temp[0]);
         if (temp.length>1) {
             String [] temp1 = temp[1].split(",");
-            ArrayList<Integer> vArrayList = new ArrayList<>();
+            // ArrayList<Integer> vArrayList = new ArrayList<>();
             for (int i = 0; i < temp1.length; i++) {
-                vArrayList.add(Integer.parseInt(temp1[i]));
+                int Values =Integer.parseInt(temp1[i]);
+                HypernymsMap.put(key,Values);
             }
-            map.put(key, vArrayList);
+            
         }
     }
-    System.out.println("hypernyms :"+map);
+    System.out.println("hypernyms :"+HypernymsMap.get(1));
 }
 
     public static void main(String[] args) throws IOException {
